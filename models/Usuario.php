6dad26cb7d2104ca8d8c89bcff1fa3dc22aa0b7c
NatalyGuallichico01 <natilu.naty@gmail.com>
2023-03-12 18:01:37 -0500

    5

--- models/Usuario.php
+++ models/Usuario.php
@@ -27,7 +27,7 @@ class Usuario extends ActiveRecord{
         $this->password=$args['password'] ?? '';
         $this->telefono=$args['telefono'] ?? '';
         $this->admin=$args['admin'] ?? '0';
-        $this->confirmado=$args['confirmado'] ?? '0';
+        $this->confirmado=$args['confirmado'] ?? '1';
         $this->token=$args['token'] ?? '';
         //$query="SELECT * FROM " . self::$tabla . " WHERE nombre = 'Patricia'";
         //debuguear($query);
--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,5 +1,6 @@
 <h1 class="nombrePagina">Creaste una cuenta</h1>
 <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
 <p class="descripcionPagina">Para poder ingresar al sistema inica sesión</p>
-<div class="boton"></div>
+<div class="boton">
 <a href="/">Iniciar Sesión</a>
+</div>
