22b55a6ea7f31c3bb2f5859180de9a26460e52b2
NatalyGuallichico01 <natilu.naty@gmail.com>
2023-03-12 17:52:42 -0500

    5

--- views/auth/mensaje.php
+++ views/auth/mensaje.php
@@ -1,4 +1,5 @@
 <h1 class="nombrePagina">Creaste una cuenta</h1>
 <?php include_once __DIR__ . '/../templates/alertas.php'; ?>
 <p class="descripcionPagina">Para poder ingresar al sistema inica sesión</p>
+<div class="boton"></div>
 <a href="/">Iniciar Sesión</a>
